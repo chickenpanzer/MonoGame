b2c695722a2d3005fcc58570c577fd35041924e3
chauvin gael <chickenpanzer@hotmail.com>
2018-09-08 15:11:03 +0200

    Ajout du projet Toolkit (UI WPF) pour création de niveaux Ajout XML parsing pour le template de niveau

---
+++ MonoGameTest/ToolKit/XML/XMLHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ToolKit
+{
+	public static class XMLHelper
+	{
+
+		public static T ReadXMLToObject<T>(string xmlFileName)
+		{
+			var result = default(T);
+
+			XmlSerializer serializer = new XmlSerializer(typeof(T));
+			using (var sr = new FileStream(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, xmlFileName), FileMode.Open))
+			{
+				result = (T)serializer.Deserialize(sr);
+			}
+
+			return result;
+		}
+
+	}
+}
